b746b38371f0dc4de2f13ab879b8f7a68616cf5a
BrunoV7 <backupnobre62@gmail.com>
2024-12-06 21:00:38 -0300

    Adiciona configuração de logger com Winston, ajusta middleware de autenticação na dashboard e atualiza configuração do servidor para HTTPS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import fs from 'fs';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -28,5 +30,15 @@ export default defineNuxtConfig({
 
   experimental: {
     // reactivityTransform: true, // Habilita macros reativas do Vue
+  },
+
+  // Configuração de servidor
+  devServer: {
+    port: 443,  // Altere para 443 para rodar em HTTPS
+    host: '0.0.0.0',  // Isso permite que o Nuxt seja acessado de fora do container
+    https: {
+      key: fs.readFileSync('/etc/ssl/private/server.key', 'utf8'),  // Caminho para sua chave privada
+      cert: fs.readFileSync('/etc/ssl/certs/server.crt', 'utf8')  // Caminho para o certificado SSL
+    }
   },
 })
